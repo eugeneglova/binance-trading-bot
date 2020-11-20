a5eaa13ba463d56aabb134daa529327df44e4776
Eugene Glova <eugene.glova@gmail.com>
2020-11-20 22:41:40 +0300

    Unsubscribe tg events

--- src/electron/tgbot.js
+++ src/electron/tgbot.js
@@ -13,26 +13,35 @@ const start = (em) => {
 
   const bot = new Telegraf(config.TELEGRAM_BOT_TOKEN)
 
-  em.on('tg:newPosition', (data) => {
+  const onNewPosition = (data) => {
     const { symbol, side, p } = data
     bot.telegram.sendMessage(config.TELEGRAM_USER_ID, `Open ${symbol} ${side}\n${parseFloat(p.entryPrice)}`)
-  })
+  }
 
-  em.on('tg:closePosition', (data) => {
+  em.on('tg:newPosition', onNewPosition)
+
+  const onClosePosition = (data) => {
     const { symbol, side, pl, count } = data
     const plValue = parseFloat(pl)
     const plSign = Math.sign(plValue) > 0 ? '+' : ''
     bot.telegram.sendMessage(config.TELEGRAM_USER_ID, `Closed ${symbol} ${side} (${count})\n${plSign}${precision(plValue)}`)
-  })
+  }
 
-  em.on('testmessage', () => {
+  em.on('tg:closePosition', onClosePosition)
+
+  const onTestMessage = () => {
     bot.telegram.sendMessage(config.TELEGRAM_USER_ID, `Test`)
-  })
+  }
+
+  em.on('testmessage', onTestMessage)
 
   bot.launch()
 
   const stop = () => {
     clearInterval(configIntervalId)
+    em.off('tg:newPosition', onNewPosition)
+    em.off('tg:closePosition', onClosePosition)
+    em.off('testmessage', onTestMessage)
     bot.stop()
   }
 
